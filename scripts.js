b1cdf5ce85b175ebc978837ad8e22b34c6402e16
user <jeffdplante@aol.com>
2022-10-25 07:32:05 -0700

    Begin creation of custom form constraint app

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello There");
+const form = document.getElementById('user-form')
+const pass1 = document.getElementById('pass1')
+const pass2 = document.getElementById('pass2')
+
+form.addEventListener ('submit', (e) => {
+
+  let messages = {};
+
+  if (pass1.value !== pass2.value) {
+    messages.pass1 = "Passwords do not match";
+  }
+  
+  if (Object.keys(messages).length > 0) {
+    e.preventDefault();
+    Object.keys(messages).forEach(key => console.log(messages.key));
+  }
+})
