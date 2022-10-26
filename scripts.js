f36b3d906f9cddbcc66b0c264f87d29b143e40f3
user <jeffdplante@aol.com>
2022-10-26 07:45:58 -0700

    Complete password check script

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
 const form = document.getElementById('user-form')
 const pass1 = document.getElementById('pass1')
 const pass2 = document.getElementById('pass2')
+const errorMessage = document.getElementById('err')
+form.noValidate = true;
 
 form.addEventListener ('submit', (e) => {
 
-  let messages = {};
-
   if (pass1.value !== pass2.value) {
-    messages.pass1 = "Passwords do not match";
-  }
-  
-  if (Object.keys(messages).length > 0) {
     e.preventDefault();
-    Object.keys(messages).forEach(key => console.log(messages.key));
+    pass1.classList.add("error");
+    pass2.classList.add("error");
+    errorMessage.classList.add("show-error");
   }
 })
